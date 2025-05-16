dbd8ec5ffef7717d2dcc2eb826d8ba8c4296c75a
Braith May <braith.may@wisetechglobal.com>
2025-05-16 12:35:25 +1000

    Remove sample site code

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Home" },
+    { name: "description", content: "Welcome to the app!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>Welcome to your new app!</div>;
 }
