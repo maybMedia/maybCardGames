c33a78eef2a78dc97f3a5150039d14b15510089d
Braith May <braith.may@wisetechglobal.com>
2025-06-10 16:53:52 +1000

    Updates

---
+++ assets/martyCrush-DNiZB89E.js
@@ -0,0 +1,19 @@
+import{w as t}from"./with-props-BW5-PigH.js";import{o as e}from"./chunk-D4RADZKF-B3oN04Lp.js";function a({}){return[{title:"Marty Crush"},{name:"description",content:"Line up Marty and his friends to clear the screen!"},{content:"user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"}]}const n=t(function(){return e.jsxs("div",{className:"flex flex-col items-center px-2 sm:px-0",children:[e.jsxs("div",{className:"container mx-auto flex flex-col items-center justify-center",children:[e.jsx("h1",{className:"text-2xl font-bold p-4 text-center text-blue-500 drop-shadow-lg",children:"Marty Crush"}),e.jsxs("div",{className:"w-full sm:w-4/6 aspect-[4/5] sm:aspect-video rainbow-bg flex flex-col items-center justify-center rounded-2xl p-2 sm:p-4 text-white relative overflow-hidden",children:[e.jsx("h1",{className:"text-2xl font-bold mb-4 text-center",children:"Coming Soon!"}),e.jsxs("p",{className:"text-lg text-center mb-4",children:["Line up Marty and his friends to clear the screen!",e.jsx("br",{}),"Stay tuned for updates!"]}),e.jsx("div",{className:"w-full h-28 relative overflow-x-visible",children:e.jsx("img",{src:"./icon.png",alt:"Icon",className:"w-24 mb-4 bg-blue-100 rounded-full shadow-lg animate-roll",style:{position:"absolute",bottom:0}})})]})]}),e.jsx("style",{children:`
+          @keyframes roll-across {
+            0% {
+              left: -6rem;
+              transform: rotate(0deg);
+            }
+            80% {
+              left: calc(100% + 2rem);
+              transform: rotate(720deg);
+            }
+            100% {
+              left: calc(100% + 2rem);
+              transform: rotate(720deg);
+            }
+          }
+          .animate-roll {
+            animation: roll-across 4s linear infinite;
+          }
+        `})]})});export{n as default,a as meta};
